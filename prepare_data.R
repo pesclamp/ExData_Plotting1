7be66c74c8292dce40266f0c5b097c4812d3dbbd
pesclamp <pabloelle@gmail.com>
2016-07-10 11:24:23 +0200

    Prepare environment check if dataframe hpc_2d exists

--- prepare_data.R
+++ prepare_data.R
@@ -11,18 +11,26 @@ if (!file.exists("data/household_power_consumption.zip")) {
 }
 
 if (!file.exists("data/household_power_consumption.txt")) {
-  unzip("data/household_power_consumption.zip")
+  unzip("data/household_power_consumption.zip",
+        exdir = "data")
 }
 
-# Read file
-hpc_all <- read.csv("data/household_power_consumption.txt",
-                    sep=";",
-                    na.strings="?")
+if (!exists("hpc_2d")) {
+  # Read file
+  hpc_all <- read.csv("data/household_power_consumption.txt",
+                      sep=";",
+                      na.strings="?")
+  
+  # Convert date and time columns
+  hpc_all <- mutate(hpc_all,
+                    DateTime = as.POSIXct(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S")))
+  hpc_all <- mutate(hpc_all,
+                    Date = as.Date(Date, "%d/%m/%Y"))
+  
+  # Subset necesary rows
+  hpc_2d <- subset(hpc_all,
+                   Date == as.Date(c("1/2/2007"), "%d/%m/%Y") | Date == as.Date(c("2/2/2007"), "%d/%m/%Y"))
 
-# Convert date and time columns
-hpc_all <- mutate(hpc_all, Date = as.Date(Date, "%d/%m/%Y"))
-hpc_all <- mutate(hpc_all, DateTime = as.POSIXct(strptime(paste(Date, Time), "%Y-%m-%d %H:%M:%S")))
-
-# Subset necesary rows
-hpc_2d <- subset(hpc_all,
-                 Date == as.Date(c("1/2/2007"), "%d/%m/%Y") | Date == as.Date(c("2/2/2007"), "%d/%m/%Y"))
+  # Clean environment
+  rm("hpc_all")
+}
